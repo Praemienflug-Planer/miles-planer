d06ccfce2b671b2ce8ed36b7979552dbe0a6699b
DomH <dominikhermanski@googlemail.com>
2026-05-10 22:55:24 +0200

    Add Amex transfer partner data

---
+++ assets/js/amex-transfer-partners.js
@@ -0,0 +1,16 @@
+window.AMEX_TRANSFER_PARTNERS = [
+  { group: "Airline-Programme", name: "The British Airways Club", unit: "Avios", ratio: "5 : 4", factor: 0.8, type: "Avios" },
+  { group: "Airline-Programme", name: "Iberia Club", unit: "Avios", ratio: "5 : 4", factor: 0.8, type: "Avios" },
+  { group: "Airline-Programme", name: "Air France/KLM Flying Blue", unit: "Flying Blue Meilen", ratio: "5 : 4", factor: 0.8, type: "Airline-Meilen" },
+  { group: "Airline-Programme", name: "SAS EuroBonus", unit: "EuroBonus Punkte", ratio: "5 : 4", factor: 0.8, type: "Airline-Punkte" },
+  { group: "Airline-Programme", name: "Cathay", unit: "Cathay Miles", ratio: "3 : 2", factor: 0.6666666667, type: "Airline-Meilen" },
+  { group: "Airline-Programme", name: "Delta SkyMiles", unit: "SkyMiles", ratio: "3 : 2", factor: 0.6666666667, type: "Airline-Meilen" },
+  { group: "Airline-Programme", name: "Etihad Guest", unit: "Etihad Guest Meilen", ratio: "3 : 2", factor: 0.6666666667, type: "Airline-Meilen" },
+  { group: "Airline-Programme", name: "Qatar Airways Privilege Club", unit: "Avios", ratio: "3 : 2", factor: 0.6666666667, type: "Avios" },
+  { group: "Airline-Programme", name: "Singapore Airlines KrisFlyer", unit: "KrisFlyer Meilen", ratio: "3 : 2", factor: 0.6666666667, type: "Airline-Meilen" },
+  { group: "Airline-Programme", name: "Emirates Skywards", unit: "Skywards Meilen", ratio: "2 : 1", factor: 0.5, type: "Airline-Meilen" },
+  { group: "Hotelprogramme", name: "Hilton Honors", unit: "Hilton Honors Punkte", ratio: "1 : 1", factor: 1, type: "Hotelpunkte" },
+  { group: "Hotelprogramme", name: "Marriott Bonvoy", unit: "Marriott Bonvoy Punkte", ratio: "3 : 2", factor: 0.6666666667, type: "Hotelpunkte" },
+  { group: "Hotelprogramme", name: "Radisson Rewards", unit: "Radisson Rewards Punkte", ratio: "1 : 2", factor: 2, type: "Hotelpunkte" },
+  { group: "Weitere Transferpartner", name: "PAYBACK", unit: "PAYBACK Punkte", ratio: "3 : 1", factor: 0.3333333333, type: "PAYBACK Punkte" }
+];
