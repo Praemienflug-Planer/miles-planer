02582599bcd45c0dd05e15f0e90448c71597c3cf
DomH <dominikhermanski@googlemail.com>
2026-05-10 22:58:52 +0200

    Add calculator dropdown master data

---
+++ assets/data/rechner-lists.js
@@ -0,0 +1,49 @@
+window.MILES_PLANNER_LISTS = {
+  dataStand: "10.05.2026",
+  source: "GitHub Stammdaten-Spiegel für Dropdowns. Live-Dropdowns laufen vorerst weiter über Google Apps Script / Google Sheets.",
+  ziele: [
+    "Dubai",
+    "Japan",
+    "Malediven",
+    "Südafrika",
+    "Thailand",
+    "USA East",
+    "USA West"
+  ],
+  reiseklassen: [
+    "Economy",
+    "Premium Economy",
+    "Business"
+  ],
+  reisezeiten: [
+    "Nebensaison",
+    "Hauptreisezeit",
+    "Ferien"
+  ],
+  reisemonate: [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+  ],
+  programme: [
+    "Miles & More",
+    "Avios",
+    "Flying Blue",
+    "KrisFlyer"
+  ],
+  szenarien: [
+    "best",
+    "realistisch",
+    "konservativ"
+  ],
+  personen: [1, 2, 3, 4, 5, 6, 7, 8]
+};
